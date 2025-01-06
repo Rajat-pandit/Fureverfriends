32ee593a2b1e812e37619328636fb89221a77fa8
Rajat Pandit <razatp2203@gmail.com>
2025-01-06 18:30:36 +0545

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {Home} from "./Components/Home";
+import {Login} from "./Components/Login";
+import {Signup} from "./Components/Signup";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

b43908b8d0047ea83c3ed3a714a54a1fe4291e28
Rajat Pandit <razatp2203@gmail.com>
2025-01-08 17:27:18 +0545

    some updated frontend in landing page

--- src/Components/Header/Web.jsx
+++ src/Components/Header/Web.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Header.css';
-
+import { useNavigate } from 'react-router-dom';
 export const Web = () => {
+  const navigate=useNavigate();
+  const getstarted=()=>{
+    navigate('/login');
+  }
   return (
     <div className='header'>
       <img src="./header.jpg" alt="Background" className="header-image" />
       <div className="header-content">
         <h2>Furever Friends</h2>
         <p>Welcome to the website. We care about your pet.</p>
+
+        <button className="btn" onClick={getstarted}> Get Started </button>
       </div>
     </div>
   );
